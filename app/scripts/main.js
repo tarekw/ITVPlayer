b329a3c9ff807de2e5922347354904019ee8d677
Tarek Wahab <tarek.wahab@orange.com>
2013-12-02 17:43:01 +0000

    Adding dependencies for backbone

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,44 @@
-console.log('\'Allo \'Allo!');
+var ITV = ITV || {};
+
+ITV.app = {
+
+	start: function() {
+		var that = this;
+
+		this.models = {
+			// search: new SearchModel()
+		};
+
+		this.collections = {
+			// searchCollection: new SearchCollection(null, {})
+		};
+
+		this.views = {
+			// searchView: new SearchView({el: '#SearchView', template: '#searchTemplate'})
+		};
+
+		// this.router = new AppRouter(this);
+
+		Backbone.history.start();
+
+		this.bindEvents();
+		this.startupRequests();
+	},
+
+	bindEvents: function() {
+		var that = this;
+
+	},
+
+	startupRequests: function() {
+		if(ITV.LOG) console.log("Starting requests...");
+
+	},
+
+};
+
+$(document).ready(function(){
+
+	ITV.app.start();
+
+});
